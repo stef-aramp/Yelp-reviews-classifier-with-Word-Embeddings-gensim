e56b38856bd06234798a450916ade121ddf73f7d
stef-aramp <johndoe@example.com>
2018-08-23 18:24:00 +0300

    push1.1

--- pre_work.py
+++ pre_work.py
@@ -14,10 +14,10 @@ import pandas as pd
 # After a few manipulations to structure I concatenated them and saved as new
 # csv file
 
-df = pd.read_csv('xaa.csv', encoding = 'utf-8', engine = 'python',error_bad_lines=False)
+df = pd.read_csv('xaa.csv', encoding = 'utf-8', engine = 'python', error_bad_lines = False)
 df = df.drop(['review_id', 'user_id', 'business_id', 'date','useful', 'funny', 'cool'], axis = 1)
 
-df2 = pd.read_csv('xac.csv', encoding = 'utf-8', engine = 'python',error_bad_lines=False,header=None)
+df2 = pd.read_csv('xac.csv', encoding = 'utf-8', engine = 'python', error_bad_lines = False, header = None)
 df2 = df2.drop([0, 1, 2, 4, 6, 7, 8], axis = 1)
 df2 = df2[1:]
 df2.columns = ['stars', 'text']
